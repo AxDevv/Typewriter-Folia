4920da907f6baa8f3a7331ed6891769576f6cf34
AxDevv <twitch.rwayl@gmail.com>
2026-01-16 06:48:36 +0100

    fix: Add scheduler parameter to run method

--- extensions/RoadNetworkExtension/src/main/kotlin/com/typewritermc/roadnetwork/entries/PathStreamDisplay.kt
+++ extensions/RoadNetworkExtension/src/main/kotlin/com/typewritermc/roadnetwork/entries/PathStreamDisplay.kt
@@ -313,7 +313,7 @@ abstract class PathStreamProducer(
 
     private fun launchOnPlayerScheduler(block: suspend () -> Unit): CancellableTask? {
         return if (FoliaSupported.isFolia) {
-            val task = player.scheduler.run(plugin, java.util.function.Consumer { _: ScheduledTask ->
+            val task = player.scheduler.run(player.scheduler, plugin, java.util.function.Consumer { _: ScheduledTask ->
                 runBlocking { block() }
             })
             task?.let { FoliaTask(it) }
